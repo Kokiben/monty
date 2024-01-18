314073a12a2c9dd58dea8a032f3f2635b24add22
Kokiben <bennassarkaoutar@gmail.com>
2024-01-18 14:36:01 -0800

    monty

--- codeofmonty.c
+++ codeofmonty.c
@@ -1,55 +1,48 @@
-#include <stdio.h>
-#include <string.h>
 #include "monty.h"
 
 /**
  * main - interpreter for custom monty code.
- * @arc: total number of arguments.
- * @arv: location of the monty file.
+ * @argc: total number of arguments.
+ * @argv: location of the monty file.
  * Return: 0 upon successful execution.
  */
 
-int main(int arc, char *arv[])
+int main(int argc, char *argv[])
 {
 char *opcode;
-FILE *data = fopen(data_path, "r")
+FILE *data;
 size_t line_buffer_size = 0;
 size_t line_length;
 unsigned int line_counter = 0;
 stack_t *execut_stack = NULL;
 
-
-if (arc != 2)
+if (argc != 2)
 {
 fprintf(stderr, "USAGE: monty file\n");
 exit(EXIT_FAILURE);
 }
-else
-{
-data = fopen(arv[1], "r");
-trans.data = data;
+else {
+data = fopen(argv[1], "r");
 
-if (!data)
-{
-fprintf(stderr, "Error: Can't open file %s\n", arv[1]);
+if (!data) {
+fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 exit(EXIT_FAILURE);
 }
 }
+
 do {
 opcode = NULL;
 line_length = getline(&opcode, &line_buffer_size, data);
-trans.opcode = opcode;
 line_counter++;
 
-if (line_length > 0)
-{
+if (line_length > 0) {
 code_execute(opcode, &execut_stack, line_counter, data);
 }
 
 free(opcode);
-} while (line_length > 0);
+} while (line_length != -1U);
 
 _freesta(execut_stack);
 fclose(data);
-return (0);
+return 0;
 }
