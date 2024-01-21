bb018ea21b88f893b6079509536b74b0b71b84f0
Kokiben <bennassarkaoutar@gmail.com>
2024-01-20 18:29:37 -0800

    o

--- codeofmonty.c
+++ codeofmonty.c
@@ -12,48 +12,44 @@ transport_t trans = {0, NULL, NULL, NULL};
 
 int main(int argc, char *argv[])
 {
-char *opcode;
-FILE *data;
-size_t line_buffer_size = 0;
-size_t line_length = 1;
-stack_t *execut_stack = NULL;
-
+char *line_content;
+FILE *monty_file;
+size_t line_size = 0;
+ssize_t line_read = 1;
+stack_t *stack = NULL;
 unsigned int line_counter = 0;
 
 if (argc != 2)
 {
 fprintf(stderr, "USAGE: monty file\n");
 exit(EXIT_FAILURE);
-} 
-else
-{
-data = fopen(argv[1], "r");
-trans.data = data;
+}
 
-if (!data)
+monty_file = fopen(argv[1], "r");
+trans.data = monty_file;
+
+if (!monty_file)
 {
 fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 exit(EXIT_FAILURE);
 }
-}
-
-do {
-opcode = NULL;
-line_length = getline(&opcode, &line_buffer_size, data);
-trans.opcode = opcode;
+while (line_read > 0)
+{
+line_content = NULL;
+line_read = getline(&line_content, &line_size, monty_file);
+trans.payload = line_content;
 line_counter++;
 
-if (line_length > 0)
+if (line_read > 0)
 {
-code_execute(opcode, &execut_stack, line_counter, data);
+code_execute(line_content, &stack, line_counter, monty_file);
 }
 
-free(opcode);
+free(line_content);
 }
-while (line_length > 0);
 
-_freesta(execut_stack);
-fclose(data);
+_freesta(stack);
+fclose(monty_file);
 
 return 0;
 }
